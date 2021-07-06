8c02709d6a1e6f1f162b5c7651ffe315d7358f16
Munchicken <spierce@austin.rr.com>
2021-07-06 13:10:26 -0500

    Added list to parse and logic to append it all together.  Seems to have some odd appostrophes...

--- excel-holiday-formula-helper.py
+++ excel-holiday-formula-helper.py
@@ -1,1 +1,13 @@
-print("Excel Holiday Formula Helper")
+"""
+Excel Holiday Formula Helper
+
+Helper app to create formula pieces for holiday formatting in Excel
+"""
+
+holidays = [ 'WBI', 'SB', 'THK', 'WBII' ] #list of holiday labels
+final = [] #output
+
+for holiday in holidays:
+    final.append( 'AND(' + holiday + '-B<>"",' + holiday + '-E<>"",B6>=' + holiday + '-B,B6<=' + holiday + '-E),')
+
+print(final)
